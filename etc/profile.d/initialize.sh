b703af087b9ef5b250ab69e96964d24699bdef3e
OliBerlin <91020645+OliBerlin@users.noreply.github.com>
2024-11-07 21:01:44 +0100

    Update initialize.sh

--- etc/profile.d/initialize.sh
+++ etc/profile.d/initialize.sh
@@ -13,7 +13,8 @@ useradd -m -G wheel,audio,users,windows-mounts -s /bin/bash $username
 passwd $username
 
 # Customzing portage
-emerge --sync --quiet
+echo "Building portage"
+emerge --sync
 nativecpu=`gcc -mtune=native -Q --help=target|grep '  -mtune=' | sed 's/  -mtune=//'| awk '{print $NF}'`
 sed -i "s/-O2/-O2 -march=$nativecpu/g" make.conf #/etc/portage/make.conf
 echo 'VIDEO_CARDS="d3d12"' >> /etc/portage/make.conf
@@ -24,7 +25,7 @@ emerge --quiet --verbose --update --deep --newuse @world
 
 # Customizing locale-gen
 cd /etc
-rm locale-gen
+rm locale.gen
 wget https://github.com/OliBerlin/gentoo-wsl/raw/refs/heads/main/etc/locale.gen
 locale-gen
 eselect locale list
@@ -37,3 +38,10 @@ wget https://github.com/OliBerlin/gentoo-wsl/raw/refs/heads/main/etc/wsl.conf
 groupid=`getent group windows-mounts | cut -d: -f3`
 sed -i "s/uid=1000,gid=1000/gid=$groupid/g" wsl.conf
 sed -i "s/default = nobody/default = $username/g" wsl.conf
+
+# sudo-stuff
+cd /etc
+mkdir sudoers.d
+cd sudoers.d
+
+rm /etc/profile.d/initialize.sh
