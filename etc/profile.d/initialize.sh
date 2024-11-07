15855bdf5d7e20b4704373dd3e96a39c177aede4
OliBerlin <91020645+OliBerlin@users.noreply.github.com>
2024-11-07 20:37:52 +0100

    Update initialize.sh

--- etc/profile.d/initialize.sh
+++ etc/profile.d/initialize.sh
@@ -1,32 +1,35 @@
 #/bin/bash
-groupadd windows-mounts
-read -p "Username: " username
-useradd -m -G wheel,audio,users,windows-mounts -s /bin/bash $username &
-wait %2
-passwd $username
-
-
+# Some systemd-stuff
 systemd-machine-id-setup
 systemd-firstboot --prompt
-
-# Customzing portage
-nativecpu=`gcc -mtune=native -Q --help=target|grep '  -mtune=' | sed 's/  -mtune=//'| awk '{print $NF}'`
-sed -i "s/-O2/-O2 -march=$nativecpu/g" make.conf #/etc/portage/make.conf
-echo 'VIDEO_CARDS="d3d12"' >> /etc/portage/make.conf
-emerge --ask --oneshot app-portage/cpuid2cpuflags
-echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags
-emerge --depclean app-portage/cpuid2cpuflags
 
 # Setting timezone
 ln -sf /usr/share/zoneinfo/Europe/Berlin  /etc/localtime
 
+# Creating user snd group
+groupadd windows-mounts
+read -p "Username: " username
+useradd -m -G wheel,audio,users,windows-mounts -s /bin/bash $username 
+passwd $username
+
+# Customzing portage
+emerge --sync --quiet
+nativecpu=`gcc -mtune=native -Q --help=target|grep '  -mtune=' | sed 's/  -mtune=//'| awk '{print $NF}'`
+sed -i "s/-O2/-O2 -march=$nativecpu/g" make.conf #/etc/portage/make.conf
+echo 'VIDEO_CARDS="d3d12"' >> /etc/portage/make.conf
+emerge --quiet --oneshot app-portage/cpuid2cpuflags
+echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags
+emerge --depclean app-portage/cpuid2cpuflags
+emerge --quiet --verbose --update --deep --newuse @world
+
 # Customizing locale-gen
 cd /etc
+rm locale-gen
 wget https://github.com/OliBerlin/gentoo-wsl/raw/refs/heads/main/etc/locale.gen
 locale-gen
 eselect locale list
 read localesetting
-eselect locale set localesetting
+eselect locale set $localesetting
 
 # Some wsl-stuff
 cd /etc
